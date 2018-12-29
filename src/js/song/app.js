d5ee6afa771a3ca6b8569f4ca49683845c2b57ec
vic <741472797@qq.com>
2018-12-29 20:26:05 +0800

    添加了播放&暂停功能

---
+++ src/js/song/app.js
@@ -0,0 +1,83 @@
+{
+    let view = {
+        el: '#app',
+        template:`
+            <audio src={{url}}></audio>
+            <div>
+                <button class="play">播放</button>
+                <button class="pause">暂停</button>
+
+            </div>
+        `,
+        render(data){
+            $(this.el).html(this.template.replace('{{url}}',data.url))
+        },
+        play(){
+            let audio = $(this.el).find('audio')[0]
+            audio.play()
+        },
+        pause(){
+            let audio = $(this.el).find('audio')[0]
+            audio.pause()
+        }
+    }
+    let model = {
+        data: {
+            id: '',
+            name: '',
+            singer: '',
+            url: ''
+        },
+        get(id) {
+            var query = new AV.Query('Song')
+            return query.get(id).then((song)=>{
+                Object.assign(this.data, {id:song.id, ...song.attributes}) 
+                return song
+            })
+        }
+
+    }
+    let controller = {
+        init(view, model) {
+            this.view = view
+            this.model = model
+            let id = this.getSongId()
+            this.model.get(id).then(()=>{
+                this.view.render(this.model.data)
+            })
+            this.bindEvents()
+        },
+        bindEvents(){
+            $(this.view.el).on('click','.play',()=>{
+                this.view.play()
+            })
+            $(this.view.el).on('click','.pause',()=>{
+                this.view.pause()
+            })
+        },
+        getSongId() {
+            let search = window.location.search
+            if (search.indexOf('?') === 0) {
+                search = search.substring(1)
+            }
+
+            let array = search.split('&').filter((v => v))//filter可以过滤出空字符串
+            let id = ''
+
+            for (let i = 0; i < array.length; i++) {
+                let keyValue = array[i].split('=')
+                let key = keyValue[0]
+                let value = keyValue[1]
+                if (key === 'id') {
+                    id = value
+                    break
+                }
+            }
+            return id
+        }
+
+    }
+    controller.init(view, model)
+}
+
+
