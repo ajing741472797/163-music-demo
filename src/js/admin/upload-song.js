8208971c2a23cb5ac411f8264b17dec19c7886b9
vic <741472797@qq.com>
2019-02-09 17:42:32 +0800

    更换数据库

--- src/js/admin/upload-song.js
+++ src/js/admin/upload-song.js
@@ -35,7 +35,7 @@
                 uptoken_url: 'http://localhost:8888/uptoken',
                 // unique_names: true, // 默认 false，key为文件名。若开启该选项，SDK为自动生成上传成功后的key（文件名）。
                 // save_key: true,   // 默认 false。若在服务端生成uptoken的上传策略中指定了 `sava_key`，则开启，SDK会忽略对key的处理
-                domain: 'http://pk10humpw.bkt.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
+                domain: 'http://pmnhfhzfw.bkt.clouddn.com/',   //bucket 域名，下载资源时用到，**必需**
                 // pk10humpw.bkt.clouddn.com
                 get_new_uptoken: false,  //设置上传文件的时候是否每次都重新获取新的token
                 max_file_size: '40mb',           //最大文件体积限制
